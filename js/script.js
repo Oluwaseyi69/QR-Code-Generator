a8a12cc4e91ce9468cbbfd27e1b4e132df7d88e6
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2024-01-06 23:43:59 +0100

    added another feature

--- js/script.js
+++ js/script.js
@@ -1,30 +1,10 @@
 const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
 
-const showSpinner = () => {
-  document.getElementById('spinner').style.display = 'block';
-};
-
-const hideSpinner = () => {
-  document.getElementById('spinner').style.display = 'none';
-};
-
-
-function generateQRCode() {
-  var dataToEncode = "";
-  var qrcode = new QRCode(document.getElementById("qrcode"), {
-      text: url,
-      width: 128,
-      height: 128
-  });
-}
-
-generateQRCode();
-
-
-
 const onGenerateSubmit = (e) => {
   e.preventDefault();
+
+  clearUI();
 
   const url = document.getElementById('url').value;
   const size = document.getElementById('size').value;
@@ -41,9 +21,58 @@ const onGenerateSubmit = (e) => {
 
       generateQRCode(url, size);
 
+      setTimeout(() => {
+        const saveUrl = qr.querySelector('img').src;
+        saveButton(saveUrl);
+      }, 50);
+
     }, 1000);
   }
 };
+
+function generateQRCode(url, size) {
+  var dataToEncode = "";
+  var qrcode = new QRCode(document.getElementById("qrcode"), {
+      text: url,
+      width: size,
+      height: size
+  });
+}
+
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if (saveLink)
+    saveLink.remove();
+}
+
+
+const showSpinner = () => {
+  document.getElementById('spinner').style.display = 'block';
+};
+
+const hideSpinner = () => {
+  document.getElementById('spinner').style.display = 'none';
+};
+
+
+
+
+// generateQRCode();
+
+
+
+const saveButton = (saveUrl) => {
+  const link = document.createElement('a')
+  link.id = 'save-link';
+  link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'save';
+  document.getElementById('generated').appendChild(link);
+}
+
+hideSpinner();
 
 
 
